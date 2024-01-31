1f142fd94c002943bf9b0c3d1d57cf751fbe064a
sourav78 <souravranjan488@gmail.com>
2024-01-31 20:23:04 +0530

    product buy added

--- controllers.js
+++ controllers.js
@@ -11,15 +11,26 @@ try {
 
 const showProducts = (req, res) => {
 
-    let allProducts = `
-        <ul>
-        ${Products.map((prod) => {
-        return `<li style="font-size: 28px">${prod.name} - ${prod.stock} - ratting ${prod.ratting}</li>`
-    }).join("")}
-        </ul>
-    `
+    let allProduct = `<table border="2" style="font-size: 24px; border-collapse: collapse">
+            <tr>
+                <th style="padding: 2px 10px">Product</th>
+                <th style="padding: 2px 10px">Price</th>
+                <th style="padding: 2px 10px">Ratting</th>
+                <th style="padding: 2px 10px">Stock</th>
+            </tr>`
 
-    res.send(allProducts)
+    Products.map(prod => {
+        allProduct = allProduct + `
+        <tr>
+            <td style="padding: 2px 10px">${prod.name}</td>
+            <td style="padding: 2px 10px">${prod.price}</td>
+            <td style="padding: 2px 10px">${prod.ratting}*</td>
+            <td style="padding: 2px 10px">${prod.stock}</td>
+        </tr>
+        `
+    })
+
+    res.send(allProduct)
 }
 
 const addProduct = (req, res) => {
@@ -43,7 +54,7 @@ const updateProduct = (req, res) => {
     const { name, updatedItem } = req.body
 
     let updatedProducts = Products.map((product) => {
-        if(product.name === name) {
+        if (product.name === name) {
             return { ...product, ...updatedItem }
         }
 
@@ -76,9 +87,28 @@ const deleteProduct = (req, res) => {
     })
 }
 
-export { 
-    addProduct, 
-    showProducts, 
+const buyProduct = (req, res) => {
+    const productName = req.params.productName
+
+    let buyProduct = Products.map(prod => {
+        return prod.name === productName ? { ...prod, stock: Number(prod.stock) - 1 } : prod
+    })
+
+    fs.writeFile('./mockdata.json', JSON.stringify(buyProduct), (err) => {
+        if (err) {
+            console.log(err);
+            res.status(500).json({ status: "Error updating product data" });
+        } else {
+            res.json({ status: "product data updated", product: productName });
+        }
+    })
+    
+}
+
+export {
+    addProduct,
+    showProducts,
     updateProduct,
-    deleteProduct
+    deleteProduct,
+    buyProduct
 }
--- index.js
+++ index.js
@@ -1,22 +1,16 @@
 import express from "express";
 
-import { addProduct, deleteProduct, showProducts, updateProduct } from "./controllers.js";
+import { addProduct, buyProduct, deleteProduct, showProducts, updateProduct } from "./controllers.js";
 
 const app = express()
 
 app.use(express.json())
 
 app.get('/', showProducts)
-
 app.post("/add-product", addProduct)
-
 app.post("/update-product", updateProduct)
-
 app.post("/delete-product", deleteProduct)
-
-app.get("/buy/:productName", (req, res) => {
-
-})
+app.get("/buy/:productName", buyProduct)
 
 app.listen(3000, () => {
     console.log(`http://localhost:3000`);
